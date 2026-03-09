9df6430b23770cad5acfc9de379f783f7b6c6bbd
gaelguerramtz <gaelguerramtz4@gmail.com>
2026-03-09 00:12:40 -0700

    Corrección de errores de sintaxis y vista funcional

--- parcial-1-poo/practica-1/usuario.php
+++ parcial-1-poo/practica-1/usuario.php
@@ -1,17 +1,13 @@
 <?php
-// Paso 2. Implementar la clase Usuario [cite: 31, 32]
 class Usuario {
-    // Atributos privados [cite: 34]
     private $nombre;
     private $correo;
 
-    // Constructor para inicializar atributos [cite: 38, 41]
     public function __construct($nombre, $correo) {
         $this->nombre = $nombre;
         $this->correo = $correo;
     }
 
-    // Métodos Getter [cite: 42]
     public function getNombre() {
         return $this->nombre;
     }
@@ -20,7 +16,6 @@ class Usuario {
         return $this->correo;
     }
 
-    // Métodos Setter [cite: 43]
     public function setNombre($nombre) {
         $this->nombre = $nombre;
     }
