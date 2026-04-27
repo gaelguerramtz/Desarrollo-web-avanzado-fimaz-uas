d7cd834a3d9f29bf4f4eb05b82793500fb9b779c
gaelguerramtz <gaelguerramtz4@gmail.com>
2026-04-27 02:52:58 -0700

    Add files via upload

---
+++ ExamenParcialUnidad2/src/Producto.php
@@ -0,0 +1,16 @@
+<?php
+class Producto {
+    public $id;
+    public $nombre;
+    public $descripcion;
+    public $existencia;
+    public $precio;
+
+    public function __construct($id=null, $nombre=null, $descripcion=null, $existencia=null, $precio=null) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->existencia = $existencia;
+        $this->precio = $precio;
+    }
+}
